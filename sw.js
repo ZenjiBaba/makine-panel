ea7e11845a72060fd55854cc28e54016c4552bc4
ETSTR <Etsgaming4216@gmail.com>
2026-02-13 02:23:03 +0300

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,3 +1,28 @@
+importScripts('https://www.gstatic.com/firebasejs/9.22.0/firebase-app-compat.js');
+importScripts('https://www.gstatic.com/firebasejs/9.22.0/firebase-messaging-compat.js');
+
+firebase.initializeApp({
+    apiKey: "AIzaSyByw1MrUACV0bphoLaw8BM7_RQj_x5_21o",
+    projectId: "makine-otomasyon-183ab",
+    messagingSenderId: "894852236387", // Firebase Console > Project Settings > Cloud Messaging > Sender ID
+    appId: "1:894852236387:web:da96448a4aaeabdc04e456" // Firebase Console > Project Settings > General > App ID
+});
+
+const messaging = firebase.messaging();
+
+// Arka planda mesaj gelirse yakala
+messaging.onBackgroundMessage(function(payload) {
+  console.log('Arka plan mesajı:', payload);
+  const notificationTitle = payload.notification.title;
+  const notificationOptions = {
+    body: payload.notification.body,
+    icon: 'https://cdn-icons-png.flaticon.com/512/13309/13309211.png',
+    vibrate: [200, 100, 200]
+  };
+
+  self.registration.showNotification(notificationTitle, notificationOptions);
+});
+
 // sw.js - Android Uyumlu Service Worker
 
 // 1. Kurulum: Beklemeden aktif ol
